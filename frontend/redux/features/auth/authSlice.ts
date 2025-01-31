ee5ec889d9231abec58c7d386e1cb2a17809b50c
MalithaJeewaka <144432834+MalithaJeewaka@users.noreply.github.com>
2025-01-31 14:37:15 +0530

    redux store provider wrapper created and auth part of redux done

---
+++ frontend/redux/features/auth/authSlice.ts
@@ -0,0 +1,67 @@
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
+
+let user;
+
+if (typeof window !== "undefined") {
+  user = JSON.parse(localStorage.getItem("user"));
+}
+
+const initialState = {
+  user: user ? user : null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+//  register new user
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error: any) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isSuccess = true;
+        state.isLoading = false;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload as string;
+        state.user = null;
+      });
+  },
+});
+
+export default authSlice.reducer;
