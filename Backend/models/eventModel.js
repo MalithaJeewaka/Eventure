0133c6761d67b6d3639576d669fe6e5305a46658
MalithaJeewaka <144432834+MalithaJeewaka@users.noreply.github.com>
2025-01-31 12:49:18 +0530

    event model created

--- Backend/models/eventModel.js
+++ Backend/models/eventModel.js
@@ -1,23 +1,53 @@
 const mongoose = require("mongoose");
 
-const eventSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please enter your event name"],
+const eventSchema = mongoose.Schema(
+  {
+    organizer: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: [true, "Please enter your event name"],
+    },
+    slogan: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: [true, "Please enter a description about the event"],
+    },
+    venue: {
+      type: String,
+      required: [true, "Please enter event location"],
+    },
+    startTime: {
+      type: String,
+      required: [true, "Please enter event start time"],
+    },
+    duration: {
+      type: String,
+      required: [true, "Please enter event duration"],
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "Concert",
+        "Drama",
+        "Conference",
+        "Festival",
+        "Sports",
+        "Charity",
+        "Seminar",
+        "Stand up",
+        "Fashion",
+        "Other",
+      ],
+    },
   },
-  slogan: {
-    type: String,
-  },
-  venue: {
-    type: String,
-    required: [true, "Please enter event location"],
-  },
-  startTime: {
-    type: String,
-    required: [true, "Please enter event start time"],
-  },
-  duration: {
-    type: String,
-    required: [true, "Please enter event duration"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
